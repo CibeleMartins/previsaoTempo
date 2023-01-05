1b821835e8865186aa5cb58537f22e4edb280be9
Cibele Martins <Cibele.Martins@ibm.com>
2023-01-05 11:37:22 -0300

    ♻️ feat: refatorando a aplicação e o componente de previsão do tempo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import axios from "axios";
 import Forecast from "../src/Forecast/Forecast";
-import { useState, useEffect } from "react";
-
+import { useState, useEffect, useMemo } from "react";
 
 function App() {
-
   const [location, setLocation] = useState();
-  const [data, setData] = useState({});
+  const [data, setData] = useState({
+    id: Math.random(parseInt()),
+    currentTemperature: 0,
+    maxTemperature: 0,
+    minTemperature: 0,
+    windSpeed: 0,
+    humidity: 0,
+    icon: "",
+    climateNow: "",
+    region: "",
+  });
 
   let getWeather = async (lat, long) => {
     let res = await axios
@@ -15,31 +23,50 @@ function App() {
       )
       .then((response) => {
         const res = response.data;
-        // console.log(res)
-        setData(res);
+        console.log(res)
+        if (Object.keys(res).length > 0) {
+          setData([
+            {
+              currentTemperature: res.main.temp,
+              maxTemperature: res.main.temp_max,
+              minTemperature: res.main.temp_min,
+              windSpeed: res.wind.speed,
+              humidity: res.main.humidity,
+              climateNow: res.weather.map((i) => i.description),
+              icon: res.weather.map((i) => i.icon),
+              region: res.name,
+            },
+          ]);
+        }
       })
       .catch((error) => {
         console.log(error.message);
       });
   };
 
-  // console.log(Object.keys(data || {}).length, data);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
       getWeather(position.coords.latitude, position.coords.longitude);
       setLocation(true);
     });
-
-    
-
   }, []);
 
-  return (
-    <div className="App">
-      <Forecast forecastData={data} />
-    </div>
-  );
+
+  if (location) {
+    return (
+      <div className="App">
+        <Forecast forecastData={data} />
+      </div>
+    );
+  } else {
+    return (
+      <h1 style={{ color: "red" }}>
+        Permita que o navegador acesse sua localização para poder verificar a
+        previsão do tempo para o seu local atual.
+      </h1>
+    );
+  }
 }
 
 export default App;
--- src/Forecast/Forecast.jsx
+++ src/Forecast/Forecast.jsx
@@ -1,114 +1,17 @@
-
 import { HStack, VStack } from "@chakra-ui/react";
 
 import style from "./Forecast.module.css";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState} from "react";
 
-import wind from '../assets/wind.png'
-import waterDrop from '../assets/waterDrop.png'
+import wind from "../assets/wind.png";
+import waterDrop from "../assets/waterDrop.png";
 
-const Forecast = ({ forecastData}) => {
-  // console.log(backgroundImage)
-  console.log(forecastData);
-
-  const [stateForecast, setForecast] = useState([
-    {
-      id: Math.random(parseInt()),
-      currentTemperature: 0,
-      maxTemperature: 0,
-      minTemperature: 0,
-      windSpeed: 0,
-      humidity: 0,
-      icon: "",
-      climateNow: "",
-      region: "",
-    },
-  ]);
-  // console.log(Object.values(stateForecast).length);
-  // console.log(stateForecast);
-
-  useEffect(() => {
-    if (Object.keys(forecastData).length > 0) {
-      setForecast([
-        {
-          currentTemperature: forecastData.main.temp,
-          maxTemperature: forecastData.main.temp_max,
-          minTemperature: forecastData.main.temp_min,
-          windSpeed: forecastData.wind.speed,
-          humidity: forecastData.main.humidity,
-          climateNow: forecastData.weather.map((i) => i.description),
-          icon: forecastData.weather.map((i) => i.icon),
-          region: forecastData.name,
-        },
-      ]);
-    }
-  }, [forecastData]);
+const Forecast = ({ forecastData }) => {
 
   return (
-    <VStack
-      className={style.animate}
-      sx={{
-        background:
-          "linear-gradient(90deg, rgba(43,108,176,1) 10%, rgba(66,153,225,1) 90%)",
-      }}
-      w="100vw"
-      h="100vh"
-      justifyContent="space-between"
-    >
-      {stateForecast.map((i) => {
-        if (
-          i.currentTemperature > 0 &&
-          i.maxTemperature > 0 &&
-          i.minTemperature > 0 &&
-          i.windSpeed > 0 &&
-          i.humidity > 0 &&
-          i.currentTemperature &&
-          i.region.length > 0 &&
-          i.icon.length > 0
-        ) {
-          const descriptionClimate =
-            i.climateNow.toString().charAt(0).toUpperCase() +
-            i.climateNow.toString().slice(1);
-          return (
-            <>
-              <HStack p="45px" h="60px" w="100%" justifyContent="flex-start">
-                <h1 className={style.climate}>Clima |</h1>{" "}
-                <h1 className={style.region}>{i.region}</h1>
-              </HStack>
 
-              <HStack w="100%" justifyContent="space-between">
-                <VStack w="50%" pl="45px" h="300px" alignItems="flex-start">
-                  <HStack>
-                    <h2 className={style.descriptionClimate}>
-                      {descriptionClimate}
-                    </h2>
-                    <img
-                      alt="icon"
-                      src={`http://openweathermap.org/img/wn/${i.icon}@2x.png`}
-                    />
-                  </HStack>
-
-                  <h1 className={style.currentTemperature}>
-                    {parseInt(i.currentTemperature) + " ºC"}
-                  </h1>
-                </VStack>
-                <VStack w="30%" h="300px" justifyContent="flex-end" alignItems="flex-end" >
-                  <HStack
-                   w="90%">
-                    <img id={style.image} alt="humidity" src={waterDrop} /> <h3 className={style.details}>{i.humidity + "%"}</h3>
-                  </HStack>
-                  <HStack
-                   w="90%">
-                    <img id={style.image} alt="wind" src={wind} /> <h3 className={style.details}>{Math.max(i.windSpeed) + "Km/h"}</h3>
-                  </HStack>
-                </VStack>
-              </HStack>
-            </>
-          );
-        }
-      })}
-    </VStack>
+    <h1>Hello</h1>
   );
 };
 
