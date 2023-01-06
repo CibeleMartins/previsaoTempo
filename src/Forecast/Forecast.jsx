e6da22c802de35170d6a05bc626adc74befd72b7
Cibele Martins <Cibele.Martins@ibm.com>
2023-01-06 13:38:04 -0300

    🚧 feat: testando algumas coisas

--- src/Forecast/Forecast.jsx
+++ src/Forecast/Forecast.jsx
@@ -1,11 +1,9 @@
-import { HStack, VStack, Text } from "@chakra-ui/react";
+import { HStack, VStack, Skeleton, Text } from "@chakra-ui/react";
 
 import style from "./Forecast.module.css";
 
-import { useEffect, useState } from "react";
-
-import wind from "../assets/wind.png";
-import waterDrop from "../assets/waterDrop.png";
+import wind from "../assets/wind.svg";
+import waterDrop from "../assets/raindrop.svg";
 
 const Forecast = ({ forecastData }) => {
   console.log(forecastData);
@@ -25,38 +23,43 @@ const Forecast = ({ forecastData }) => {
       h="100vh"
       className={style.animate}
     >
-      <HStack w="100%" h="20vh" alignItems="center">
-        <h1 style={{ paddingLeft: "5%" }} className={style.mainTitle}>
-          Clima
-        </h1>{" "}
-        <h1 className={style.mainTitle}>|</h1>
-        <h3 className={style.region}>{forecastData.region}</h3>
+      <HStack w="100%" h={["10vh", "10vh","20vh","20vh","20vh","20vh"]} alignItems="center">
+        <Text fontSize={['30px', '40px', '50px','50px','50px','50px']} style={{ paddingLeft: "5%" }} className={style.mainTitle}>
+          Clima |
+        </Text>
+
+        <Text fontSize={['20px', '30px', '37px','37px','37px','37px']} className={style.region}>{forecastData.region}</Text>
       </HStack>
-      <HStack w="100%" h="40vh" justifyContent="space-between">
-        <VStack w="50%">
+
+      <HStack w="100%" h={['30vh', '40vh', '50vh', '50vh', '50vh', '50vh']} justifyContent="space-between" alignItems="center">
+        <VStack w={["100%","100%","50%","50%","50%","50%"]} h='27vh' justifyContent="center">
           <HStack w="100%" pl="10%">
-            <h2 className={style.description}>{descriptionClimate}</h2>
+            <Text fontSize={['30px','40px','40px','40px','40px','40px']} className={style.description}>{descriptionClimate}</Text>
             <img
               src={icon}
               className={style.iconDescription}
               alt="temperatureDescription"
             />
           </HStack>
-          <h1 className={style.temperature}>{currentTemp}</h1>
+          <Text fontSize={['40px', '80px', '100px','100px','100px','100px']} className={style.temperature}>{currentTemp}</Text>
         </VStack>
+
         <VStack
         pr="5%"
         w="50%"
-        alignItems="flex-end">
+        h={['20vh', '20vh', '27vh', '27vh', '27vh', '27vh']}
+        justifyContent={['flex-end', 'flex-end','flex-end','flex-end','flex-end','flex-end']}
+        alignItems={['flex-start', 'flex-start', 'flex-end', 'flex-end', 'flex-end', 'flex-end']}
+        alignContent="center">
           <HStack
           w="25%">
             <img alt="humidity" className={style.humidityImage} src={waterDrop}/>
-            <h1 id={style.details}>{humidity}</h1>
+            <Text fontSize={['10px','15px','20px','20px','20px','20px']} id={style.details}>{humidity}</Text>
           </HStack>
           <HStack
            w="25%">
             <img alt="wind" className={style.windImage} src={wind}/>
-            <h1 id={style.details}>{windSpeed}</h1>
+            <Text fontSize={['10px','15px','20px','20px','20px','20px']} id={style.details}>{windSpeed}</Text>
           </HStack>
         </VStack>
       </HStack>
--- src/Loading/Loading.jsx
+++ src/Loading/Loading.jsx
@@ -1,16 +1,17 @@
 import { Center } from "@chakra-ui/react";
 
 // video and image
-import video from "../../media/solar.mp4";
-import styled from "@emotion/styled";
+import video from "../media/solar.mp4";
+import logo from '../assets/logo.svg'
 
-import style from './Loding.module.css';
+import style from './Loading.module.css';
 
 const Loading = () => {
   return (
-    <Center w="100%" h="100vh" bg="black">
+    <Center w="100%" h="100vh">
       <div className={style.overlay}></div>
-      <video src={video} autoPlay loop muted />
+      <video className={style.video} src={video} autoPlay loop muted />
+      <img alt="logo" className={style.svgPosition} src={logo}/>
     </Center>
   );
 };
